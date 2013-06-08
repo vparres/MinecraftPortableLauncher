30a696ccd2a2fd76db0cd90f19cb913810dbef8e
Vincent PARRES <vparres@vparres.com>
2013-06-08 16:19:52 +0200

    Added Binary

---
+++ bin/bin/path.cmd
@@ -0,0 +1,15 @@
+@echo off
+echo Setting up APPDATA Path ...
+set APPDATA=\data
+echo Path set !
+echo ==================================================
+echo Resuming launching of Minecraft Launcher ...  
+bin\Minecraft.exe
+echo Done. 
+echo ==================================================
+echo Script by VPARRES. Version 1.5.2
+echo ==================================================
+echo You can now close this Window
+echo Press any key to close the launcher.
+pause
+exit
